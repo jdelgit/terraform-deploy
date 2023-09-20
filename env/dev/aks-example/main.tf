1438d30ae5ebf0c34faeb25fd547bfc73c0c2a0e
deladev <deladev@noreply.gitea.local>
2023-09-20 18:38:52 +0000

    Add deploy operation and pub-ip for VM (#4)
    You can now pass create or destroy to set your desired state

--- env/dev/aks-example/main.tf
+++ env/dev/aks-example/main.tf
@@ -29,28 +29,28 @@ locals {
 
 }
 
-# resource "azurerm_resource_group" "environment_resource_group" {
-#   name     = "__runtimeGroupName__"
-#   location = "__location__"
-#   tags     = local.tags
-# }
+resource "azurerm_resource_group" "environment_resource_group" {
+  name     = "__runtimeGroupName__"
+  location = "__location__"
+  tags     = local.tags
+}
 
-# module "k8scluster" {
-#   source                          = "./../../../../terraform-modules/azure/aks"
-#   resource_group_name             = azurerm_resource_group.environment_resource_group.name
-#   resource_group_location         = azurerm_resource_group.environment_resource_group.location
-#   cluster_name                    = "__clusterName__"
-#   cluster_admin_group_ids         = ["__clusterAdminGroupAADId__"]
-#   cluster_dns_prefix              = "__clusterDnsPrefix__"
-#   enable_auto_scaling             = "__clusterAutoScale__"
-#   node_count                      = "__clusterNodeCount__"
-#   min_count                       = "__clusterMinNodeCount__"
-#   max_count                       = "__clusterMaxModeCount__"
-#   private_cluster_enabled         = "__clusterPrivateNetwork__"
-#   public_network_access_enabled   = "__clusterPubNetAccessEnabled__"
-#   load_balancer_sku               = "__clusterLoadBalancerSku___"
-#   cluster_sku_tier                = "__clusterSkuTier___"
-#   api_server_authorized_ip_ranges = "__clusterAuthorizedIps__"
-#   kubernetes_version              = "__clusterKubernetesVersion__"
-#   tags                            = local.tags
-# }
+module "k8scluster" {
+  source                          = "./../../../../terraform-modules/azure/aks"
+  resource_group_name             = azurerm_resource_group.environment_resource_group.name
+  resource_group_location         = azurerm_resource_group.environment_resource_group.location
+  cluster_name                    = "__clusterName__"
+  cluster_admin_group_ids         = ["__clusterAdminGroupAADId__"]
+  cluster_dns_prefix              = "__clusterDnsPrefix__"
+  enable_auto_scaling             = "__clusterAutoScale__"
+  node_count                      = "__clusterNodeCount__"
+  min_count                       = "__clusterMinNodeCount__"
+  max_count                       = "__clusterMaxModeCount__"
+  private_cluster_enabled         = "__clusterPrivateNetwork__"
+  public_network_access_enabled   = "__clusterPubNetAccessEnabled__"
+  load_balancer_sku               = "__clusterLoadBalancerSku___"
+  cluster_sku_tier                = "__clusterSkuTier___"
+  api_server_authorized_ip_ranges = "__clusterAuthorizedIps__"
+  kubernetes_version              = "__clusterKubernetesVersion__"
+  tags                            = local.tags
+}
--- env/dev/vm-example/main.tf
+++ env/dev/vm-example/main.tf
@@ -40,41 +40,45 @@ locals {
   }
 }
 
-# resource "azurerm_resource_group" "environment_resource_group" {
-#   name     = "__runtimeGroupName__"
-#   location = "__location__"
-#   tags     = local.tags
-# }
+resource "azurerm_resource_group" "environment_resource_group" {
+  name     = "__runtimeGroupName__"
+  location = "__location__"
+  tags     = local.tags
+}
 
-# module "network_setup" {
-#   source                      = "./../../../../terraform-modules/azure/network"
-#   resource_group_name         = azurerm_resource_group.environment_resource_group.name
-#   resource_group_location     = azurerm_resource_group.environment_resource_group.location
-#   vnet_name                   = "__deploymentPrefix__-vnet"
-#   vnet_address_space          = "10.10.0.0/16"
-#   vm_subnet_name              = "__deploymentPrefix__-subnet"
-#   vm_subnet_address_prefix    = "10.10.1.0/24"
-#   network_security_group_name = "__deploymentPrefix__-nsg"
-#   nsgrules                    = local.nsg_rules
-#   tags                        = local.tags
-# }
+module "network_setup" {
+  source                      = "./../../../../terraform-modules/azure/network"
+  resource_group_name         = azurerm_resource_group.environment_resource_group.name
+  resource_group_location     = azurerm_resource_group.environment_resource_group.location
+  vnet_name                   = "__deploymentPrefix__-vnet"
+  vnet_address_space          = "10.10.0.0/16"
+  vm_subnet_name              = "__deploymentPrefix__-subnet"
+  vm_subnet_address_prefix    = "10.10.1.0/24"
+  network_security_group_name = "__deploymentPrefix__-nsg"
+  nsgrules                    = local.nsg_rules
+  tags                        = local.tags
+}
 
-# module "vm_setup" {
-#   source                  = "./../../../../terraform-modules/azure/virtualmachine"
-#   resource_group_name     = azurerm_resource_group.environment_resource_group.name
-#   resource_group_location = azurerm_resource_group.environment_resource_group.location
-#   vm_publisher            = "__virtualMachineOSDistribution__"
-#   vm_offer                = "__virtualMachineOSOffer__"
-#   vm_sku                  = "__virtualMachineOSSku__"
-#   vm_name                 = "__deploymentPrefix__-vm"
-#   vm_size                 = "__virtualMachineSize__"
-#   admin_name              = "__adminUserName__"
-#   admin_pubkey            = "__adminSSHKey__"
-#   vm_storage_account_type = "__storageAccountType__"
-#   vm_version              = "latest"
-#   nic_name                = "__deploymentPrefix__-nic"
-#   vm_nic_config           = "__deploymentPrefix__-nic-config"
-#   vm_private_ip           = "10.10.1.15"
-#   vm_subnet_id            = module.network_setup.vm_subnet_id
-#   tags                    = local.tags
-# }
+module "vm_setup" {
+  source                        = "./../../../../terraform-modules/azure/virtualmachine"
+  resource_group_name           = azurerm_resource_group.environment_resource_group.name
+  resource_group_location       = azurerm_resource_group.environment_resource_group.location
+  vm_publisher                  = "__virtualMachineOSDistribution__"
+  vm_offer                      = "__virtualMachineOSOffer__"
+  vm_sku                        = "__virtualMachineOSSku__"
+  vm_name                       = "__deploymentPrefix__-vm"
+  vm_size                       = "__virtualMachineSize__"
+  admin_name                    = "__adminUserName__"
+  admin_pubkey                  = "__adminSSHKey__"
+  vm_storage_account_type       = "__storageAccountType__"
+  vm_version                    = "latest"
+  nic_name                      = "__deploymentPrefix__-nic"
+  vm_nic_config                 = "__deploymentPrefix__-nic-config"
+  vm_private_ip                 = "10.10.1.15"
+  vm_subnet_id                  = module.network_setup.vm_subnet_id
+  enable_public_ip              = "__PublicIPEnabled__"
+  pubip_sku                     = "__PublicIpSku__"
+  pubip_allocation_method       = "__PublicIPAllocationMethod__"
+  privateip_allocation_method   = "__PrivateIPAllocationMethod__"
+  tags                          = local.tags
+}
